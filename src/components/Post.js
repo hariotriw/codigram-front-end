416191a3028b9c5009834034ec775f4e96e50fd3
Wowo <wowovanbothen@gmail.com>
2022-05-19 16:02:46 +0700

    commit halaman home

--- src/components/Post.js
+++ src/components/Post.js
@@ -9,9 +9,11 @@ const Post = () => {
                 <div className="mx-auto d-flex py-2 mb-2">
                     <img src = "https://via.placeholder.com/150" className ="rounded-circle me-3 d-flex" alt = "Rounded Image" style={{ width: "50px" }}/>
                     {/* <p className="d-flex fs-5 text-dark ms-2 d-flex">nama akun</p> */}
-                    <p className="fs-5 text-dark me-auto d-flex my-auto">harioTriWibowo</p>
-                    <i class="bi bi-three-dots me-4 d-flex my-auto" style={{ fontSize: "1.5rem" }}></i>
-                    {/* <i class="bi bi-gear-fill me-4 d-flex my-auto" style={{ fontSize: "1.5rem" }}></i> */}
+                    <Link className="no-link  me-auto d-flex my-auto" to="/a">
+                        <p className="fs-5 text-dark me-auto d-flex my-auto ">harioTriWibowo</p>
+                    </Link>
+                    <i className="bi bi-three-dots me-4 d-flex my-auto" style={{ fontSize: "1.5rem" }}></i>
+                    {/* <i className="bi bi-gear-fill me-4 d-flex my-auto" style={{ fontSize: "1.5rem" }}></i> */}
                 </div>
             </div>
             {/* --- Image section */}
@@ -21,19 +23,19 @@ const Post = () => {
             {/* --- Content section */}
             <div className="row p-0 m-0">
                 <div className="mx-auto d-flex pt-4">
-                    {/* <i class="bi bi-heart me-3 d-flex my-auto shadow" style={{ fontSize: "1.5rem" }}></i> */}
+                    {/* <i className="bi bi-heart me-3 d-flex my-auto shadow" style={{ fontSize: "1.5rem" }}></i> */}
 
-                    <i class="bi bi-heart-fill me-3 d-flex my-auto shadow" style={{ fontSize: "1.5rem", color: "red" }}></i>
-                    {/* <i class="bi bi-chat me-3 d-flex my-auto" style={{ fontSize: "1.5rem" }}></i> */}
-                    <div class="input-group input-group-sm">
-                        <input type="text" class="form-control" placeholder="Click here to add comment..." />
-                        <span class="input-group-text" id="basic-addon2">
-                            <i class="bi bi-chat-dots my-auto" style={{ fontSize: "1.5rem" }}></i>
+                    <i className="bi bi-heart-fill me-3 d-flex my-auto shadow" style={{ fontSize: "1.5rem", color: "red" }}></i>
+                    {/* <i className="bi bi-chat me-3 d-flex my-auto" style={{ fontSize: "1.5rem" }}></i> */}
+                    <div className="input-group input-group-sm">
+                        <input type="text" className="form-control" placeholder="Click here to add comment..." />
+                        <span className="input-group-text" id="basic-addon2">
+                            <i className="bi bi-chat-dots my-auto" style={{ fontSize: "1.5rem" }}></i>
                         </span>
                     </div>
 
-                    {/* <i class="bi bi-three-dots me-auto d-flex my-auto" style={{ fontSize: "1.5rem" }}></i> */}
-                    {/* <i class="bi bi-gear-fill me-4 d-flex my-auto" style={{ fontSize: "1.5rem" }}></i> */}
+                    {/* <i className="bi bi-three-dots me-auto d-flex my-auto" style={{ fontSize: "1.5rem" }}></i> */}
+                    {/* <i className="bi bi-gear-fill me-4 d-flex my-auto" style={{ fontSize: "1.5rem" }}></i> */}
                 </div>
                 <div className="d-flex py-1 my-0 fw-bold">
         			<p className="py-0 my-0 fs-6">100 likes</p>
@@ -43,9 +45,17 @@ const Post = () => {
                         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe quis, exercitationem veritatis maxime itaque ratione, eaque iure accusantium minus ad mollitia incidunt vel! Animi, eius dignissimos illo temporibus amet quidem hic, velit accusantium fugiat, pariatur incidunt laboriosam nam necessitatibus dolorum. Ullam necessitatibus minus nulla quae maxime accusantium fuga, totam numquam iusto? Quos incidunt quo voluptas amet consequuntur facere sint harum nihil! Iste eius blanditiis itaque porro modi corrupti tempore quam, maxime esse velit molestias.
                     </p>
                 </div>
-                <div className="d-flex py-1 my-0 mb-3">
+                <div className="d-flex py-1 my-0 mb-3 mx-auto">
         			{/* <p className="py-0 my-0 fs-6">View all <span className="fw-bold">5 Comments</span></p> */}
-        			<p className="py-0 my-0 fs-6">No Comments</p>
+                    <div className="py-0 my-0 me-auto">
+            			<p className=" fs-6 ">No Comments</p>
+                    </div>
+                    <Link className="no-link my-0 mx-auto" to="/b">
+        			    <p className="py-0 px-4 fs-6   text-dark">Click to view detail</p>
+                    </Link>
+                    <div className="py-0 my-0  ms-auto me-3">
+        			    <p className="fs-6">4 bulan yang lalu</p>
+                    </div>
                 </div>
             </div>
             {/* --- Add Comment section */}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -16,17 +16,63 @@ const HomePage = () => {
 					<div className="col-2">
 						
 					</div>
-					<div className="col-8 bg-snow">
+					<div className="col-8">
 						<div className="row p-0">
 							{/* ----- Left side content ----- */}
-							<div className="col-8 p-0" style={{ height: "30rem" }}> 
+							<div className="col-8 p-0" style={{  }}> 
+							{/* <div className="col-8 p-0" style={{ height: "30rem" }}>  */}
 								<Post />
 								<Post />
 								<Post />
 							</div>
 							{/* ----- Right side content ----- */}
-							<div className="col-4 p-1">
-								a
+							<div className="col-4 p-0">
+								{/* <div className="container-fluid ps-3 mb-4 bg-snow border border-dark"> */}
+								<div className="container-fluid ps-3">
+									<div className="bg-snow">
+										<div className="row p-0 m-0">
+											<div className="mx-auto d-flex py-1 mt-3">
+												<p className="fs-6 text-dark ms-auto my-0">Your account</p>
+											</div>
+										</div>
+										{/* --- Account information section */}
+										<Link className="no-link" to="/a">
+											<div className="row p-0 m-0">
+												<div className="mx-auto d-flex py-2 mb-2">
+													<img src = "https://via.placeholder.com/150" className ="rounded-circle me-3 d-flex" alt = "Rounded Image" style={{ width: "50px" }}/>
+													<p className="fs-5 fw-bold text-dark me-auto my-auto">hariot</p>
+												</div>
+											</div>
+											<div className="row p-0 m-0">
+												<div className="mx-auto d-flex py-1">
+													<p className="fs-5 text-dark me-auto my-auto">Hario Tri Wibowo</p>
+												</div>
+											</div>
+											<div className="row p-0 m-0">
+												<div className="mx-auto d-flex py-1 mb-2">
+													<p className="py-0 fs-6 text-justify text-break text-dark">
+														Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe quis, exercitationem veritatis maxime itaque ratione, eaque iure accusantium minus ad mollitia incidunt vel! Animi, eius dignissimos illo temporibus a
+													</p>
+												</div>
+											</div>
+										</Link>
+									</div>
+									<div className="bg-snow mt-4">
+										<Link className="no-link" to="/a">
+											<div className="row p-0 m-0">
+												<div className="mx-auto d-flex py-1 mb-2">
+													<p className="fs-5 text-dark me-auto my-auto">Suggestion For You</p>
+												</div>
+											</div>
+											<div className="row p-0 m-0">
+												<div className="mx-auto d-flex py-2 mb-2">
+													<img src = "https://via.placeholder.com/150" className ="rounded-circle me-3 d-flex" alt = "Rounded Image" style={{ width: "30px" }}/>
+													<p className="fs-6 fw-bold text-dark me-auto my-auto">another_hariot</p>
+												</div>
+											</div>
+										</Link>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
